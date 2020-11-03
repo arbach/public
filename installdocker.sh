f7e409ee53fe7d18ebd36d3d09f41bd05c572bba
arbach <dev@dev.com>
2020-11-02 19:34:12 -0500

    installdocker

---
+++ installdocker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get -y remove docker docker-engine docker.io containerd runc 
+
+sudo apt-get update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl git \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+su - $USER
+
+sudo apt install -y docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose  
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+sudo apt-get install openjdk-11-jdk
